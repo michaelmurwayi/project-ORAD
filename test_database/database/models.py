852191e65f5a8bb4a281b01e2e1002d785aff2ea
Muchai248 <muchaimark6@gmail.com>
2024-01-14 20:36:01 +0300

    new commit

---
+++ test_database/database/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.TimeField()
+    end_date = models.DateTimeField()
