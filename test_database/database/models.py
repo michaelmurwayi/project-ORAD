eff0e20fccd142a5140470e71231f63480f4c788
Muchai248 <muchaimark6@gmail.com>
2024-01-16 23:26:44 +0300

    new

--- test_database/database/models.py
+++ test_database/database/models.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 
 # Create your models here.
 
+from django.db import models
+
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -32,3 +34,4 @@ class Architecture(models.Model):
 
     def __str__(self):
         return f"{self.project.id}-{self.name}"
+
