bb63f1ed4c8c64d0dc3b57adf637f7130efb5504
Muchai248 <muchaimark6@gmail.com>
2024-01-30 13:43:42 +0300

    new commit

--- ORAD group/database/admin.py
+++ ORAD group/database/admin.py
@@ -9,6 +9,6 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_filter = ['is_active', 'is_staff']
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ['title', 'uploader', 'uploaded_at']  # Fields to display in the admin list view
+    list_display = ['title', 'uploader', 'file', 'uploaded_at']  # Fields to display in the admin list view
     search_fields = ['title']  # Fields to enable search functionality in the admin interface
     list_filter = ['uploader', 'uploaded_at']  # Fields to filter results in the admin interface
