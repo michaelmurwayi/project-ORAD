e840f93f51c661b8a071b4718b26d80816fa5bfa
Muchai248 <muchaimark6@gmail.com>
2024-01-31 22:45:04 +0300

    new frontend

--- ORAD group/test_database/settings.py
+++ ORAD group/test_database/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-oz-#@ik&a8&l#*7y2=9%3b3m0z8lph1b&kdnjerdr0ylx8tv)x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')],
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ORAD group/test_database/urls.py
+++ ORAD group/test_database/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('database.urls'))
+    path('', include('database.urls')),
 ]
