df47dee543c69688f97deb700864f0090b6206a9
Muchai248 <muchaimark6@gmail.com>
2024-02-01 04:06:08 +0300

    new commit

--- ORAD group/test_database/settings.py
+++ ORAD group/test_database/settings.py
@@ -121,10 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIR = [os.path.join(BASE_DIR, "static")
-                   
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = [os.path.join(BASE_DIR, "static/")],
 
 
 
